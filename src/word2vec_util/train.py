66dcc73f7d0a25a17b1f5bbe0af08dbe3dc2c30f
Miguel Cabrera <m.cabrera@gini.net>
2014-04-02 23:10:34 +0200

    Modified function for training with worddocuments to normalize the data added more utility functions

--- src/word2vec_util/train.py
+++ src/word2vec_util/train.py
@@ -6,13 +6,15 @@ from sklearn.svm import SVC
 
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.grid_search import GridSearchCV
+from sklearn.preprocessing  import MinMaxScaler
 
+
+from .util import create_data_vectors
 
 #let's define a function to define to train the model
 
-
-def train_with_wordvectors(model_files, data, train_index=None, test_index=None, kernel='linear'):
-""" Train a counte word vector with a word2vec model for document classiification and return the accuracies on t        he testing set"""
+def train_with_wordvectors(model_files, data, labels, train_index=None, test_index=None, kernel='linear', n_jobs=8):
+    """ Train a counte word vector with a word2vec model for document classiification and return the accuracies on t        he testing set"""
 
     import copy
         
@@ -30,13 +32,13 @@ def train_with_wordvectors(model_files, data, train_index=None, test_index=None,
     predictions =  {}
     classifiers = {}
     accuracies = {}
-
+    predictions_train = {}
+    
     if(train_index is None): 
         # So training = testing = None
         train_index = np.arange(0,data.shape[0])
         
-    if(test_index is None):
-        test_index = train_index
+    if(test_index is None):        test_index = train_index
         
     #X_train = data[train_index]
     y_train = labels[train_index]
@@ -51,18 +53,24 @@ def train_with_wordvectors(model_files, data, train_index=None, test_index=None,
         model =  word2vec.Word2Vec.load_word2vec_format(w_model, binary=True)
         data_dense = create_data_vectors(data,model)
         
+        scaler = MinMaxScaler(copy=False)
+        data_dense = scaler.fit_transform(data_dense)
+        
+        
         X_train = data_dense[train_index]
         X_test =  data_dense[test_index]
                    
        
-        clf =  svm.SVC()
         cv = StratifiedKFold(y=y_train, n_folds=2)
-        grid = GridSearchCV(SVC(kernel=kernel,class_weight='auto' ), param_grid=param_grid, cv=cv,verbose=0)
+        grid = GridSearchCV(SVC(kernel=kernel,class_weight='auto' ), param_grid=param_grid, 
+                            cv=cv,verbose=0,n_jobs=n_jobs)
         grid.fit(X_train, y_train)
         
         clf = grid.best_estimator_
         classifiers[w_model] =  copy.deepcopy(grid.best_estimator_)
         predictions[w_model] = clf.predict(X_test)
+        predictions_train[w_model] = clf.predict(X_train)
+
         #print(y_test)
         #print(predictions[w_model])
         
@@ -70,7 +78,7 @@ def train_with_wordvectors(model_files, data, train_index=None, test_index=None,
         accuracies[w_model] =  metrics.classification_report(y_test  , predictions[w_model])   
         
         
-        print("The best classifier is: ",classifiers[w_model])
+        print("The best classifier is: ", classifiers[w_model])
         
         
-    return classifiers,predictions,accuracies
+    return classifiers,predictions,accuracies,predictions_train
--- src/word2vec_util/util.py
+++ src/word2vec_util/util.py
@@ -1,4 +1,8 @@
 import numpy as np
+import json
+from os import listdir
+from os.path import isfile, join
+
 
 def load_word2vec_vocab(path):
     """ loads a word2vec vocab from the file system.
@@ -20,12 +24,79 @@ def load_word2vec_vocab(path):
     return vocab
 
 # let's define a method to do this:
-def get_doc_vector(vector,model):
+def get_doc_vector(vector, model):
     """ Returns the docter the word vector mean representation"""
     idx = vector.todense() == 1 
     doc_matrix = model.syn0[np.array(idx.flat)]
     
     return np.mean(doc_matrix,axis=0)
+
+
+#Let's define a funciton to create 
+def create_data_vectors(data, word_model):
+    """ Create data vectors from a binary term vector  
+        data: a binary term vector representation of the documents. One row per document.
+        word_mode: the word2vec wordvector model to get the wordvector from
+    """
+    data_dense  = np.zeros((data.shape[0],word_model.syn0.shape[1]))
+    #get the document vectors
+    for i,item  in enumerate(data):
+        #print("What? %d" % i)
+        data_dense[i] = get_doc_vector(item,word_model)
+        
+        #if(  (np.isfinite(data_dense[i]).all()) ):
+            #print (documents[i])
+            #print (data_dense[i])
+    return data_dense
+
+
+def load_model_descriptions(json_files):
+    """ load model description from a list of json files"""
+    
+    parameters = dict()
+    
+    for parameter_file in json_files:
+        with open(parameter_file,'r') as input:
+            runs = json.load(input)
+            
+            for run_code in runs:
+                #print "Reading meta info from  %s " % run_code
+                output = "dewiki-" + run_code + ".bin"
+                parameters[output] = runs[run_code]
+        
+    return parameters
+    
+    
+def load_documents_and_labels(data_dir, categories):
+    """ Load the document of the labels.
+    data_dir: the directoy where the documents are stored. The documents should be stored
+    in subdirectories - where the name of the directory is the label.
+    categoris: is the list of the labels or categories that is going to be extracted from the data_dir
+    directory. A subdirectoy of 'data_dir' should exists for each cateogry in 'category'.
+
+    return the list of documents, the labels and a dictionary cat : list_of_files
+    """
+    files_cat = {}
+    labels = []
+
+    for cat in categories:
+        path = join(data_dir,cat)
+        files_cat[cat] = [ join(path,f) for f in listdir(path) if isfile(join(path,f)) ]
+        labels.extend([cat] * len(files_cat[cat]))
+
+    documents = []
+    for cat in categories:
+        documents.extend(files_cat[cat])
+    
+
+    documents = np.asarray(documents)
+    labels = np.asarray(labels)
+
+    return documents, labels, files_cat
+    
+    
+
+
 
 
 if __name__ == '__main__':
